5a27585032a08a169adc0b94a034c5ee3068e600
Joe Bugajski <yogurtmystery@gmail.com>
2019-08-19 17:27:51 -0500

    queue finished

--- queue/queue.py
+++ queue/queue.py
@@ -1,9 +1,42 @@
-class Queue:
+# class Queue: # Using an array
+#   def __init__(self):
+#     self.size = 0
+#     # what data structure should we
+#     # use to store queue elements?
+#     self.read = 0
+#     self.write = 0
+#     self.storage = [] 
+
+#   def enqueue(self, item):
+#     if self.read == self.write:
+#         self.storage.clear()
+#         self.storage.append(item)
+#         self.write = 1
+#         self.read = 0
+#     else:
+#         self.storage.append(item)
+#         self.write += 1
+  
+#   def dequeue(self):
+#     if self.read == self.write:
+#         return None
+#     else:
+#         x = self.storage[self.read]
+#         self.read += 1
+#         return x
+
+#   def len(self):
+#     if self.read == self.write:
+#         return 0
+#     else:
+#         return self.write - self.read
+
+class Queue: # Using a linked-list
   def __init__(self):
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage = [] 
 
   def enqueue(self, item):
     pass
